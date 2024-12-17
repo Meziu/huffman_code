6cc499189bc8f3cafd94574614026e8ef5cff43b
meziu210@icloud.com <meziu210@icloud.com>
2024-12-17 20:32:11 +0100

    Formatting

--- include/alphabet.h
+++ include/alphabet.h
@@ -3,15 +3,15 @@
 
 #include <assert.h>
 #include <stdbool.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_ALPHABET_LENGTH 64
 #define MAX_MESSAGE_LENGTH 4096
 
 typedef struct {
 	unsigned long bitfield; // espandibile modificando il tipo
-	char length; // lunghezza del codice (da destra verso sinistra)
+	char length;			// lunghezza del codice (da destra verso sinistra)
 } Code;
 
 typedef struct SSymbol {
@@ -21,29 +21,30 @@ typedef struct SSymbol {
 } Symbol;
 
 typedef struct {
-	Symbol** symbols; // Array di riferimenti a simboli, che sono allocati dinamicamente
+	Symbol **symbols; // Array di riferimenti a simboli, che sono allocati
+					  // dinamicamente
 	unsigned int length;
 	unsigned int message_length;
 } Alphabet;
 
-Alphabet create_alphabet(char* sample_message);
-void destroy_alphabet(Alphabet* ab);
+Alphabet create_alphabet(char *sample_message);
+void destroy_alphabet(Alphabet *ab);
 
-Symbol* find_symbol(Alphabet* ab, char c);
-unsigned int sort_last_symbol(Symbol** symbols, unsigned int length);
+Symbol *find_symbol(Alphabet *ab, char c);
+unsigned int sort_last_symbol(Symbol **symbols, unsigned int length);
 
 Code new_code();
-void push_code_digit(Code* code, bool d);
+void push_code_digit(Code *code, bool d);
 
-void print_alphabet(Alphabet* ab);
-void print_symbol(Alphabet* ab, Symbol* s);
-void print_code(Code* code);
+void print_alphabet(Alphabet *ab);
+void print_symbol(Alphabet *ab, Symbol *s);
+void print_code(Code *code);
 
-float sum_of_probabilities(Alphabet* ab);
+float sum_of_probabilities(Alphabet *ab);
 
 void swap_symbol_references(Symbol **a, Symbol **b);
 void quicksort_alphabet(Alphabet *ab);
 int partition_symbols_prob(Symbol **symbols, int start, int end);
-void quicksort_symbols_prob(Symbol** symbols, int start, int end);
+void quicksort_symbols_prob(Symbol **symbols, int start, int end);
 
 #endif
--- src/huffman.c
+++ src/huffman.c
@@ -1,13 +1,13 @@
-#include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include "huffman.h"
 #include "alphabet.h"
+#include "huffman.h"
 
-void huffman_recursive(Symbol** symbols, unsigned int length);
+void huffman_recursive(Symbol **symbols, unsigned int length);
 
-void huffman_code(Alphabet* ab) {
+void huffman_code(Alphabet *ab) {
 	// Alfabeto ordinato, alfabeto fortunato
 	quicksort_alphabet(ab);
 
@@ -15,33 +15,34 @@ void huffman_code(Alphabet* ab) {
 }
 
 // Richiede che l'array symbols sia già ordinato (per la probabilità)
-void huffman_recursive(Symbol** symbols, unsigned int length) {
+void huffman_recursive(Symbol **symbols, unsigned int length) {
 	if (symbols == NULL || length <= 1) {
 		return;
 	}
 
 	if (length > 2) {
-		Symbol* second_to_last = symbols[length-2];
-		Symbol* last = symbols[length-1];
+		Symbol *second_to_last = symbols[length - 2];
+		Symbol *last = symbols[length - 1];
 
-		// L'elemento second_to_last si presta come fusione di se stesso e dell' "ultimo"
+		// L'elemento second_to_last si presta come fusione di se stesso e dell'
+		// "ultimo"
 		second_to_last->prob += last->prob;
 
-		unsigned int spot = sort_last_symbol(symbols, length-1);
+		unsigned int spot = sort_last_symbol(symbols, length - 1);
 		second_to_last = symbols[spot];
 
 		// Il risultato del codice dato da questa chiamata è restituito.
-		huffman_recursive(symbols, length-1);
+		huffman_recursive(symbols, length - 1);
 
 		second_to_last->prob -= last->prob;
 
 		// Propagazione del codice associato
 		last->code = second_to_last->code;
-
 		push_code_digit(&second_to_last->code, 1);
 		push_code_digit(&last->code, 0);
 	} else { // Caso in cui ci siano esattamente due elementi nella lista.
-		// È sempre valido il presupposto che la lista sia ordinata, quindi il primo avrà probabilità maggiore.
+		// È sempre valido il presupposto che la lista sia ordinata, quindi il
+		// primo avrà probabilità maggiore.
 		symbols[0]->code = new_code();
 		push_code_digit(&symbols[0]->code, 1);
 
